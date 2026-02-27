ce6ac2dca44338d3407c5f9537de005800295a6e
Asmit Sharma <asmitsharma543@gmail.com>
2026-02-27 20:48:59 +0530

    Refined luxury homepage design and fixed image config

--- storefront/next.config.ts
+++ storefront/next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  reactStrictMode: true,
 
-export default nextConfig;
+  images: {
+    domains: [
+      "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      "localhost",
+    ],
+  },
+
+  experimental: {
+    serverActions: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
