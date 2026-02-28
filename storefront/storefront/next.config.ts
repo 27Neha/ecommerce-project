39e6c053cfc70b2965409d80a9fd0d8a503baa12
YourGitHubUsername <youremail@example.com>
2026-02-28 12:44:39 +0530

    Resolve merge conflict in next.config.ts

---
+++ storefront/storefront/next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "9000",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
